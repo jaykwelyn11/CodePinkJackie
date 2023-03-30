0d8b01f9b9323f38fdb12339ab68e48dbfcf1191
jaykwelyn11 <jackie.sepulveda94@gmail.com>
2023-03-30 16:50:03 -0500

    project setup

---
+++ Models/MyContext.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+
+using Microsoft.EntityFrameworkCore;
+namespace CodePinkJackie.Models;
+
+public class MyContext : DbContext
+{
+    public MyContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
